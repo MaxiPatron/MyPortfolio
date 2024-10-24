c38b0b9df3ed9e6646dd646854293104c1ca3426
Maximo Patron <108689781+MaxiPatron@users.noreply.github.com>
2024-10-24 02:26:58 -0300

    Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+import Home from "./routes/Main";
+import About from "./routes/About";
+import Project from "./routes/Projects";
+import Contact from "./routes/Contact";
+
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return <div>
+    <h1>Hello World!</h1>
+  </div>
+}
+
+export default App;
